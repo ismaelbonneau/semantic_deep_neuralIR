1b1d9157b622ed4575b19aee653fb6a97e499d7e
Ismael Bonneau <ismael95210@gmail.com>
2019-07-01 12:10:33 +0200

    clean du fichier datasets.py

--- datasets.py
+++ datasets.py
@@ -5,40 +5,6 @@
 # Ismael Bonneau
 ##########################################################
 
-import os
-from bs4 import BeautifulSoup
-
-from parser_text import get_docno, get_text, get_title
-
-
-def bulking(docs, index_name):
-	bulk_data = []
-	for doc in docs:
-		data_dict = {
-				'_index': index_name,
-				'_id': get_docno(doc),
-				'_source': {
-					"text": get_text(doc),
-					"title": get_title(doc)
-				}
-			}
-		bulk_data.append(data_dict)
-	return bulk_data
-
-def collect_all_data(datasetpath, collections, index_name='robust-2004'):
-
-	listedocs = []
-	for collection in collections:
-		for root, dirs, files in os.walk(datasetpath+os.sep+collection, topdown=True):
-			for name in files:
-				with open(os.path.join(root, name), "r") as f:
-					filecontent = f.read()
-					soup = BeautifulSoup(filecontent, "html.parser")
-					docs = soup.find_all("doc")
-
-					yield bulking(docs, index_name)
-
-
 import numpy as np
 import gensim
 import random
@@ -46,7 +12,6 @@ import ast
 import json
 import os
 import pickle
-from bs4 import BeautifulSoup
 from os import listdir,sep
 from os.path import isfile, join
 from gensim.parsing.preprocessing import preprocess_string,remove_stopwords,strip_numeric, strip_tags, strip_punctuation, strip_short
@@ -66,6 +31,11 @@ def custom_tokenizer(s):
 
 
 class Dataset:
+
+	def __init__(self):
+		pass
+
+class Robust04(Dataset):
 
 	def __init__(self, intervals, model_wv, normalize=False):
 		"""
